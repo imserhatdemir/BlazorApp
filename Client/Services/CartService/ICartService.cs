ab178bbdacb44beeadf60f47b0ec58554d05eb37
Serhat Demir <64663453+wzarok@users.noreply.github.com>
2022-10-31 02:38:36 +0300

    Blazor WebAssembly in Action - Completed

--- Client/Services/CartService/ICartService.cs
+++ Client/Services/CartService/ICartService.cs
@@ -5,5 +5,8 @@
         event Action OnChange;
         Task AddToCart(CartItem cartItem);
         Task<List<CartItem>> GetCartItem();
+        Task <List<CartProductResponse>> GetCartProduct();
+        Task RemoveProductFromCart(int productId,int productTypeId);
+        Task UpdateQuantity(CartProductResponse product);
     }
 }
--- Server/Services/CartService/CartService.cs
+++ Server/Services/CartService/CartService.cs
@@ -43,7 +43,8 @@
                     ImageUrl=product.ImageURL,
                     Price=productVariant.Price,
                     ProductType= productVariant.ProductType.Name,
-                    ProductTypeId= productVariant.ProductTypeId
+                    ProductTypeId= productVariant.ProductTypeId,
+                    Quantity=item.Quantity
                 };
 
                 result.Data.Add(cartProduct);
