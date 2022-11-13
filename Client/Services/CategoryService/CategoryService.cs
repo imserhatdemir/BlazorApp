4b5e0da1340e2833466036ce3039f7e05a56ed1f
Serhat Demir <64663453+wzarok@users.noreply.github.com>
2022-11-14 00:36:18 +0300

    Categories CRUD operations with API

--- Client/Services/CategoryService/CategoryService.cs
+++ Client/Services/CategoryService/CategoryService.cs
@@ -1,4 +1,6 @@
-﻿namespace BlazorApp.Client.Services.CategoryService
+﻿using BlazorApp.Shared;
+
+namespace BlazorApp.Client.Services.CategoryService
 {
     public class CategoryService : ICategoryService
     {
@@ -9,12 +11,54 @@
             _http = http;
         }
         public List<Category> Categories { get; set; } = new List<Category>();
+        public List<Category> AdminCategories { get; set; } = new List<Category>();
+
+        public event Action OnChange;
+
+        public async Task AddCategories(Category category)
+        {
+            var response = await _http.PostAsJsonAsync("api/Category/admin",category);
+            AdminCategories = (await response.Content.ReadFromJsonAsync<ServiceResponse<List<Category>>>()).Data;
+            await GetCategories();
+            OnChange.Invoke();
+        }
+
+        public Category CreateNewCategory()
+        {
+            var newCategory = new Category { IsNew = true,Editing=true};
+            AdminCategories.Add(newCategory);
+            OnChange.Invoke();
+            return newCategory;
+        }
+
+        public async Task DeleteCategories(int categoryId)
+        {
+            var response = await _http.DeleteAsync($"api/Category/admin/{categoryId}");
+            AdminCategories = (await response.Content.ReadFromJsonAsync<ServiceResponse<List<Category>>>()).Data;
+            await GetCategories();
+            OnChange.Invoke();
+        }
+
+        public async Task GetAdminCategories()
+        {
+            var response = await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/Category/admin");
+            if (response != null && response.Data != null)
+                AdminCategories = response.Data;
+        }
 
         public async Task GetCategories()
         {
             var response = await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/Category");
             if (response != null && response.Data != null)
             Categories = response.Data;
+        }
+
+        public async Task UpdateCategories(Category category)
+        {
+            var response = await _http.PutAsJsonAsync("api/Category/admin",category);
+            AdminCategories = (await response.Content.ReadFromJsonAsync<ServiceResponse<List<Category>>>()).Data;
+            await GetCategories();
+            OnChange.Invoke();
         }
     }
 }
--- Client/Services/CategoryService/ICategoryService.cs
+++ Client/Services/CategoryService/ICategoryService.cs
@@ -2,8 +2,17 @@
 {
     public interface ICategoryService
     {
+
+
+        event Action OnChange;
         List<Category> Categories { get; set; }
+        List<Category> AdminCategories { get; set; }
         Task GetCategories();
+        Task GetAdminCategories();
+        Task DeleteCategories(int categoryId);
+        Task UpdateCategories(Category category);
+        Task AddCategories(Category category);
+        Category CreateNewCategory();
         
     }
 }
